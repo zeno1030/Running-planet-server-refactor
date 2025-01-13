4f765b481490e45bec12ed8f0ceb0f106ca5a672
Kwon Ji Hwan <zeno1030@naver.com>
2025-01-13 18:44:49 +0900

    Repository 분리

---
+++ src/main/java/clofi/runningplanet/planet/repository/PlanetRepositoryImpl.java
@@ -0,0 +1,9 @@
+package clofi.runningplanet.planet.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PlanetRepositoryImpl {
+}
